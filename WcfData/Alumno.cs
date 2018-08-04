18258e688164cc7e09efe43042247c7cd766e50e
Jenny <fantazmit@gmail.com>
2018-08-04 23:34:28 +0200

    Project

---
+++ WcfData/Alumno.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WcfData
+{
+    [ServiceContract]
+    public class Alumno
+    {
+        [DataMember]
+        public string Nombre { get; set; }
+        [DataMember]
+        public string Apellidos { get; set; }
+    }
+}
